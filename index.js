77273b81f3eb8afdc79ab67fda4c79ab287eb41f
storyteller <shashanksharma580S@gmail.com>
2018-09-13 10:31:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/attendance", function(req, res) {
+    var userId;
+    var password;
+    var speech =
+        req.body.queryResult &&
+        req.body.queryResult.parameters &&
+        req.body.queryResult.parameters.userId &&
+        req.body.queryResult.parameters.password ?
+        (userId = req.body.queryResult.parameters.userId) &&
+        (password = req.body.queryResult.parameters.password) &&
+        (speech = userId + password) :
+        "Seems like some problem. Speak again.";
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: "attendace-tracker-sample"
+    });
+});
+
+
+restService.listen(process.env.PORT || 8000, function() {
+    console.log("Server up and listening");
+});
