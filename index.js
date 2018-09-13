d0efde678fbf3167bbca251702a4358bfed996b8
storyteller <shashanksharma580S@gmail.com>
2018-09-13 23:17:24 +0530

    response json8

--- index.js
+++ index.js
@@ -27,75 +27,55 @@ restService.post("/attendance", function(req, res) {
         "Seems like some problem. Speak again.";
 
     return res.json({
-        "responseId": "f6b87d21-ec48-4399-9f80-b8bb89867c4a",
-        "queryResult": {
-            "queryText": "w",
-            "action": "userId",
-            "parameters": {
-                "password": "w",
-                "userId": "r"
+        "conversationToken": "[]",
+        "expectUserResponse": true,
+        "expectedInputs": [{
+            "inputPrompt": {
+                "richInitialPrompt": {
+                    "items": [{
+                        "simpleResponse": {
+                            "textToSpeech": "this is attendance"
+                        }
+                    }]
+                }
             },
-            "allRequiredParamsPresent": true,
-            "fulfillmentMessages": [{
-                    "platform": "ACTIONS_ON_GOOGLE",
-                    "simpleResponses": {
-                        "simpleResponses": [{
-                            "textToSpeech": "this is your attendance"
-                        }]
-                    }
-                },
-                {
-                    "platform": "ACTIONS_ON_GOOGLE",
+            "possibleIntents": [{
+                "intent": "actions.intent.OPTION",
+                "inputValueData": {
                     "carouselSelect": {
                         "items": [{
-                                "info": {
-                                    "key": "option 1",
-                                    "synonyms": [
-                                        "synnoym"
-                                    ]
+                                "optionInfo": {
+                                    "key": "option",
+                                    "synonyms": []
                                 },
                                 "title": "Title",
-                                "description": "asdasfs \\n sdsd",
-                                "image": {
-                                    "imageUri": "https://via.placeholder.com/128x232",
-                                    "accessibilityText": "sasas"
-                                }
+                                "description": "nbkbb"
                             },
                             {
-                                "info": {
-                                    "key": "option 2"
+                                "optionInfo": {
+                                    "key": "option 2",
+                                    "synonyms": []
                                 },
                                 "title": "Title 2",
-                                "description": "asdasdasdafaf",
-                                "image": {
-                                    "imageUri": "https://via.placeholder.com/128x232",
-                                    "accessibilityText": "sSA"
-                                }
-                            },
-                            {
-                                "info": {
-                                    "key": "option 3"
-                                },
-                                "title": "Title 3",
-                                "description": "ascdascc",
-                                "image": {
-                                    "imageUri": "https://via.placeholder.com/128x232",
-                                    "accessibilityText": "sass"
-                                }
+                                "description": "rghbrhrrh"
                             }
                         ]
-                    }
+                    },
+                    "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
                 }
-            ],
-            "intent": {
-                "name": "projects/technojam-app/agent/intents/68135175-be79-4648-8752-48e9b6f432b5",
-                "displayName": "userId"
+            }]
+        }],
+        "responseMetadata": {
+            "status": {
+                "message": "Success (200)"
             },
-            "intentDetectionConfidence": 1,
-            "diagnosticInfo": {
-                "end_conversation": true
-            },
-            "languageCode": "en"
+            "queryMatchInfo": {
+                "queryMatched": true,
+                "intent": "68135175-be79-4648-8752-48e9b6f432b5",
+                "parameterNames": [
+                    "password"
+                ]
+            }
         }
     });
 
