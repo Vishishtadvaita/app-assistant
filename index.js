c5c1f1dad7f60cc39ed4195695f3eaf47a7fc483
storyteller <shashanksharma580S@gmail.com>
2018-09-13 17:23:21 +0530

    response json4

--- index.js
+++ index.js
@@ -25,21 +25,79 @@ restService.post("/attendance", function(req, res) {
         (password = req.body.queryResult.parameters.password) &&
         (speech = userId + password) :
         "Seems like some problem. Speak again.";
+
     return res.json({
-        payload: {
-            google: {
-                expectUserResponse: false,
-                richResponse: {
-                    items: [{
-                        simpleResponse: {
-                            textToSpeech: "Alright, your silly name is red 25! I hope you like it. See you next time."
-                        }
-                    }]
-                },
-                userStorage: "{\"data\":{}}"
-            }
-        }
+        "conversationToken": "",
+        "expectUserResponse": true,
+        "expectedInputs": [{
+            "inputPrompt": {
+                "initialPrompts": [{
+                    "textToSpeech": "Alright! Here are a few things you can learn. Which sounds interesting?"
+                }],
+                "noInputPrompts": []
+            },
+            "possibleIntents": [{
+                "intent": "actions.intent.OPTION",
+                "inputValueData": {
+                    "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                    "carouselSelect": {
+                        "items": [{
+                                "optionInfo": {
+                                    "key": "MATH_AND_PRIME",
+                                    "synonyms": [
+                                        "math",
+                                        "math and prime",
+                                        "prime numbers",
+                                        "prime"
+                                    ]
+                                },
+                                "title": "Math & prime numbers",
+                                "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
+                                "image": {
+                                    "url": "http://example.com/math_and_prime.jpg",
+                                    "accessibilityText": "Math & prime numbers"
+                                }
+                            },
+                            {
+                                "optionInfo": {
+                                    "key": "EGYPT",
+                                    "synonyms": [
+                                        "religion",
+                                        "egpyt",
+                                        "ancient egyptian"
+                                    ]
+                                },
+                                "title": "Ancient Egyptian religion",
+                                "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
+                                "image": {
+                                    "url": "http://example.com/egypt",
+                                    "accessibilityText": "Egypt"
+                                }
+                            },
+                            {
+                                "optionInfo": {
+                                    "key": "RECIPES",
+                                    "synonyms": [
+                                        "recipes",
+                                        "recipe",
+                                        "42 recipes"
+                                    ]
+                                },
+                                "title": "42 recipes with 42 ingredients",
+                                "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
+                                "image": {
+                                    "url": "http://example.com/recipe",
+                                    "accessibilityText": "Recipe"
+                                }
+                            }
+                        ]
+                    }
+                }
+            }]
+        }]
     });
+
+
 
 
 });
