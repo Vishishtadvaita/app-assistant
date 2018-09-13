db936848e34ed2bdb02140485bbba15524eb4143
storyteller <shashanksharma580S@gmail.com>
2018-09-13 18:40:26 +0530

    response json5

--- index.js
+++ index.js
@@ -27,73 +27,51 @@ restService.post("/attendance", function(req, res) {
         "Seems like some problem. Speak again.";
 
     return res.json({
-        "conversationToken": "",
-        "expectUserResponse": true,
-        "expectedInputs": [{
-            "inputPrompt": {
-                "initialPrompts": [{
-                    "textToSpeech": "Alright! Here are a few things you can learn. Which sounds interesting?"
-                }],
-                "noInputPrompts": []
-            },
-            "possibleIntents": [{
-                "intent": "actions.intent.OPTION",
-                "inputValueData": {
-                    "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                    "carouselSelect": {
-                        "items": [{
-                                "optionInfo": {
-                                    "key": "MATH_AND_PRIME",
-                                    "synonyms": [
-                                        "math",
-                                        "math and prime",
-                                        "prime numbers",
-                                        "prime"
-                                    ]
-                                },
-                                "title": "Math & prime numbers",
-                                "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                                "image": {
-                                    "url": "http://example.com/math_and_prime.jpg",
-                                    "accessibilityText": "Math & prime numbers"
-                                }
-                            },
-                            {
-                                "optionInfo": {
-                                    "key": "EGYPT",
-                                    "synonyms": [
-                                        "religion",
-                                        "egpyt",
-                                        "ancient egyptian"
-                                    ]
-                                },
-                                "title": "Ancient Egyptian religion",
-                                "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                                "image": {
-                                    "url": "http://example.com/egypt",
-                                    "accessibilityText": "Egypt"
-                                }
-                            },
-                            {
-                                "optionInfo": {
-                                    "key": "RECIPES",
-                                    "synonyms": [
-                                        "recipes",
-                                        "recipe",
-                                        "42 recipes"
-                                    ]
-                                },
-                                "title": "42 recipes with 42 ingredients",
-                                "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                                "image": {
-                                    "url": "http://example.com/recipe",
-                                    "accessibilityText": "Recipe"
-                                }
-                            }
+        "data": {
+            "google": {
+                "expect_user_response": true,
+                // "permissions_request": "not sure what goes here, I usually leave it out"
+            }
+        },
+        "messages": [{
+            "items": [{
+                    "description": "Option One Description",
+                    "image": {
+                        "url": "https://via.placeholder.com/140x100"
+                    },
+                    "optionInfo": {
+                        "key": "itemOne",
+                        "synonyms": [
+                            "thing one",
+                            "object one"
                         ]
-                    }
+                    },
+                    "title": "Option One Title"
+                },
+                {
+                    "description": "Option Two Description",
+                    "image": {
+                        "url": "https://via.placeholder.com/140x100"
+                    },
+                    "optionInfo": {
+                        "key": "itemTwo",
+                        "synonyms": [
+                            "thing two",
+                            "object two"
+                        ]
+                    },
+                    "title": "Option Two Title"
                 }
-            }]
+            ],
+            "platform": "google",
+            "type": "carousel_card"
+        }],
+        "contextOut": [{
+            "name": "carousel",
+            "lifespan": 5,
+            "parameters": {
+                "followup-event": "hero_selected"
+            }
         }]
     });
 
