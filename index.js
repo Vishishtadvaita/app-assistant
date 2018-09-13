004286e05f090f0e9af64de66aa119aedc041710
storyteller <shashanksharma580S@gmail.com>
2018-09-13 23:05:49 +0530

    response json9

--- index.js
+++ index.js
@@ -27,61 +27,75 @@ restService.post("/attendance", function(req, res) {
         "Seems like some problem. Speak again.";
 
     return res.json({
-        "speech": "Hello",
-        "contextOut": [{
-            "name": "_actions_on_google_",
-            "lifespan": 100,
-            "parameters": {}
-        }],
-        "data": {
-            "google": {
-                "expectUserResponse": true,
-                "richResponse": {
-                    "items": [{
-                        "simpleResponse": {
-                            "textToSpeech": "Hello"
-                        }
-                    }],
-                    "suggestions": []
+        "responseId": "f6b87d21-ec48-4399-9f80-b8bb89867c4a",
+        "queryResult": {
+            "queryText": "w",
+            "action": "userId",
+            "parameters": {
+                "password": "w",
+                "userId": "r"
+            },
+            "allRequiredParamsPresent": true,
+            "fulfillmentMessages": [{
+                    "platform": "ACTIONS_ON_GOOGLE",
+                    "simpleResponses": {
+                        "simpleResponses": [{
+                            "textToSpeech": "this is your attendance"
+                        }]
+                    }
                 },
-                "systemIntent": {
-                    "intent": "actions.intent.OPTION",
-                    "data": {
-                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                        "carouselSelect": {
-                            "items": [{
-                                    "title": "Foo",
-                                    "image": {
-                                        "url": "http://example.com/foo.jpg",
-                                        "accessibilityText": "Foo title"
-                                    },
-                                    "optionInfo": {
-                                        "key": "foo-key",
-                                        "synonyms": [
-                                            "foo-alt-1",
-                                            "foo-alt-2"
-                                        ]
-                                    }
+                {
+                    "platform": "ACTIONS_ON_GOOGLE",
+                    "carouselSelect": {
+                        "items": [{
+                                "info": {
+                                    "key": "option 1",
+                                    "synonyms": [
+                                        "synnoym"
+                                    ]
                                 },
-                                {
-                                    "title": "Bar",
-                                    "image": {
-                                        "url": "http://example.com/bar.jpg",
-                                        "accessibilityText": "Bar title"
-                                    },
-                                    "optionInfo": {
-                                        "key": "bar-key",
-                                        "synonyms": [
-                                            "bar-alt-1",
-                                            "bar-alt-2"
-                                        ]
-                                    }
+                                "title": "Title",
+                                "description": "asdasfs \\n sdsd",
+                                "image": {
+                                    "imageUri": "https://via.placeholder.com/128x232",
+                                    "accessibilityText": "sasas"
                                 }
-                            ]
-                        }
+                            },
+                            {
+                                "info": {
+                                    "key": "option 2"
+                                },
+                                "title": "Title 2",
+                                "description": "asdasdasdafaf",
+                                "image": {
+                                    "imageUri": "https://via.placeholder.com/128x232",
+                                    "accessibilityText": "sSA"
+                                }
+                            },
+                            {
+                                "info": {
+                                    "key": "option 3"
+                                },
+                                "title": "Title 3",
+                                "description": "ascdascc",
+                                "image": {
+                                    "imageUri": "https://via.placeholder.com/128x232",
+                                    "accessibilityText": "sass"
+                                }
+                            }
+                        ]
                     }
                 }
-            }
+            ],
+            "intent": {
+                "name": "projects/technojam-app/agent/intents/68135175-be79-4648-8752-48e9b6f432b5",
+                "displayName": "userId"
+            },
+            "intentDetectionConfidence": 1,
+            "diagnosticInfo": {
+                "end_conversation": true
+            },
+            "languageCode": "en"
         }
     });
 
