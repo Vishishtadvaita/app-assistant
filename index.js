8a17e3ffb981e0cfab891b317a48da2b00b9c7c1
storyteller <shashanksharma580S@gmail.com>
2018-09-14 14:31:32 +0530

    fullfilliment

--- index.js
+++ index.js
@@ -25,63 +25,11 @@ restService.post("/attendance", function(req, res) {
         (password = req.body.queryResult.parameters.password) &&
         (speech = userId + password) :
         "Seems like some problem. Speak again.";
-
     return res.json({
-        "conversationToken": "[]",
-        "expectUserResponse": true,
-        "expectedInputs": [{
-            "inputPrompt": {
-                "richInitialPrompt": {
-                    "items": [{
-                        "simpleResponse": {
-                            "textToSpeech": "this is attendance"
-                        }
-                    }]
-                }
-            },
-            "possibleIntents": [{
-                "intent": "actions.intent.OPTION",
-                "inputValueData": {
-                    "carouselSelect": {
-                        "items": [{
-                                "optionInfo": {
-                                    "key": "option",
-                                    "synonyms": []
-                                },
-                                "title": "Title",
-                                "description": "nbkbb"
-                            },
-                            {
-                                "optionInfo": {
-                                    "key": "option 2",
-                                    "synonyms": []
-                                },
-                                "title": "Title 2",
-                                "description": "rghbrhrrh"
-                            }
-                        ]
-                    },
-                    "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec"
-                }
-            }]
-        }],
-        "responseMetadata": {
-            "status": {
-                "message": "Success (200)"
-            },
-            "queryMatchInfo": {
-                "queryMatched": true,
-                "intent": "68135175-be79-4648-8752-48e9b6f432b5",
-                "parameterNames": [
-                    "password"
-                ]
-            }
-        }
+        fulfillmentText: speech,
+        displayText: speech,
+        source: "attendace-tracker-sample"
     });
-
-
-
-
 });
 
 
